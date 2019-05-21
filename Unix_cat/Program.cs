1edd9cb8502e92249d72f1e5639ae362473bd9fd
kovalikvictoria <49001451+kovalikvictoria@users.noreply.github.com>
2019-05-21 14:55:45 +0300

    Initial commit

---
+++ Unix_cat/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unix_cat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
