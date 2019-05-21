d62fa997390a505ff339ffa8d53208496e885941
kovalikvictoria <49001451+kovalikvictoria@users.noreply.github.com>
2019-05-21 17:11:41 +0300

    Add new solutions

---
+++ Unix_cat/Concrete/Cat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unix_cat.General;
+
+namespace Commands.Concrete
+{
+    public class Cat : Command
+    {
+        public override int Execute()
+        {
+
+        }
+    }
+}
